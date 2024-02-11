8fe917c9a085b1ed2106c95a44c13e8f419bb5cd
Liliana <lilkupczyk@gmail.com>
2024-02-11 23:27:57 +0100

    code simplification, modified generatePass

--- src/App.jsx
+++ src/App.jsx
@@ -4,147 +4,46 @@ import './App.css'
 function App() {
 	const [isPassGenerate, setIsPassGenerate] = useState(false)
 	const [newPass, setNewPass] = useState('')
-	const [passLength, setPassLength] = useState('4')
+	const [hiddenPass, setHiddenPass] = useState('')
+	const [passLength, setPassLength] = useState(4)
 	const [isSpecialChar, setIsSpecialChar] = useState(false)
 	const [isNumbers, setIsNumbers] = useState(false)
 	const [isUppercase, setIsUppercase] = useState(false)
-	const [numberOfChecks, setNumberOfChecks] = useState(1)
-	let passwordArr = []
-	let genLetter, genCharacter, genNumber, genUpperLetter
-	const letters = [
-		'a',
-		'b',
-		'c',
-		'd',
-		'e',
-		'f',
-		'g',
-		'h',
-		'i',
-		'j',
-		'k',
-		'l',
-		'm',
-		'n',
-		'o',
-		'p',
-		'r',
-		's',
-		't',
-		'u',
-		'v',
-		'x',
-		'y',
-		'z',
-	]
-	const characters = [
-		'~',
-		'`',
-		'!',
-		'@',
-		'#',
-		'$',
-		'%',
-		'^',
-		'&',
-		'*',
-		'(',
-		')',
-		'_',
-		'-',
-		'+',
-		'=',
-		'{',
-		'}',
-		'[',
-		']',
-		';',
-		':',
-		"'",
-		'"',
-		'|',
-		'/',
-		'<',
-		'>',
-		',',
-		'.',
-		'?',
-	]
-	const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-	let allSignArr = []
+	const [isPassShown, setIsPassShown] = useState(true)
+	// const [errorMessage, setErrorMessage] = useState('')
 
-	const generatePass = (passLength, signs, nums, ucase) => {
-		let capitalizeArr = letters.map(letter => letter.toUpperCase())
+	const generatePass = () => {
+		const letters = 'abcdefghijklmnopqrstuvwxyz'
+		const characters = '~`!@#$%^&*()_-+={}[];:|?<>,?'
+		const numbers = '0123456789'
+		const UpLetters = letters.toUpperCase()
+		let tabChecks = 0
+		let chosenSigns = letters
+		let genPass = []
 
-		genCharacter = characters[Math.floor(Math.random() * characters.length)]
-		genNumber = numbers[Math.floor(Math.random() * numbers.length)]
-		genLetter = letters[Math.floor(Math.random() * letters.length)]
-		genUpperLetter = letters[Math.floor(Math.random() * letters.length)].toUpperCase()
+		setIsPassShown(true)
+		setHiddenPass('')
 
-		console.log(`numberOfChecks: ${numberOfChecks}`)
-		if (passLength) {
-			if (signs === true && nums === true && ucase === true) {
-				passwordArr.push(genUpperLetter, genCharacter, genNumber)
-				allSignArr.push(...letters, ...numbers, ...characters, ...capitalizeArr)
-			} else if (signs === true && nums === true) {
-				passwordArr.push(genCharacter, genNumber)
-				allSignArr.push(...letters, ...numbers, ...characters)
-			} else if (signs === true && ucase === true) {
-				passwordArr.push(genCharacter, genUpperLetter)
-				allSignArr.push(...letters, ...characters, ...capitalizeArr)
-			} else if (nums === true && ucase === true) {
-				passwordArr.push(genNumber, genUpperLetter)
-				allSignArr.push(...letters, ...numbers, ...capitalizeArr)
-			} else if (signs === true) {
-				passwordArr.push(genCharacter)
-				allSignArr.push(...letters, ...characters)
-			} else if (nums === true) {
-				passwordArr.push(genNumber)
-				allSignArr.push(...letters, ...numbers)
-			} else if (ucase === true) {
-				passwordArr.push(genUpperLetter)
-				allSignArr.push(...letters, ...capitalizeArr)
-			} else {
-				passwordArr.push(genLetter)
-				allSignArr.push(...letters)
-			}
+		if (isSpecialChar) {
+			chosenSigns += characters
+			genPass.push(characters[Math.floor(Math.random() * characters.length)])
+			tabChecks++
 		}
-		const flatArr = allSignArr.flat(Infinity)
-		for (let i = 0; i < passLength - numberOfChecks; i++) {
-			passwordArr.push(flatArr[Math.floor(Math.random() * flatArr.length)])
-			passwordArr.sort(() => {
-				return 0.5 - Math.random()
-			})
+		if (isNumbers) {
+			chosenSigns += numbers
+			genPass.push(numbers[Math.floor(Math.random() * numbers.length)])
+			tabChecks++
 		}
-		setNewPass(passwordArr)
-	}
+		if (isUppercase) {
+			chosenSigns += UpLetters
+			genPass.push(UpLetters[Math.floor(Math.random() * UpLetters.length)])
+			tabChecks++
+		}
 
-	const countChecks = () => {
-		console.log(isSpecialChar)
-		console.log(isNumbers)
-		console.log(isUppercase)
-		if (isSpecialChar === true && isNumbers === true && isUppercase === true) {
-			setNumberOfChecks(prevNumber => {
-				return prevNumber === 0 ? prevNumber + 3 : 3
-			})
-		} else if (
-			(isSpecialChar === true && isNumbers === true) ||
-			(isSpecialChar === true && isUppercase === true) ||
-			(isNumbers === true && isUppercase === true)
-		) {
-			setNumberOfChecks(prevNumber => {
-				return prevNumber === 0 ? prevNumber + 2 : 2
-			})
-		} else if (isSpecialChar === false && isNumbers === false && isUppercase === false) {
-			setNumberOfChecks(prevNumber => {
-				return prevNumber === 0 ? prevNumber + 1 : 1
-			})
-		} else {
-			setNumberOfChecks(prevNumber => {
-				return prevNumber === 0 ? prevNumber + 1 : 1
-			})
+		for (let i = 0; i < passLength - tabChecks; i++) {
+			genPass.push(chosenSigns[Math.floor(Math.random() * chosenSigns.length)])
 		}
-		console.log(`numOfChecks ${numberOfChecks}`)
+		setNewPass(genPass)
 	}
 
 	return (
@@ -156,7 +55,6 @@ function App() {
 						type='number'
 						value={passLength}
 						min='4'
-						max='15'
 						step='1'
 						id='length-id'
 						onChange={e => {
@@ -169,6 +67,7 @@ function App() {
 						<input
 							type='checkbox'
 							id='special-id'
+							checked={isSpecialChar}
 							onChange={() => {
 								setIsSpecialChar(prev => !prev)
 							}}
@@ -179,6 +78,7 @@ function App() {
 						<input
 							type='checkbox'
 							id='numbers-id'
+							checked={isNumbers}
 							onChange={() => {
 								setIsNumbers(prev => !prev)
 							}}
@@ -189,6 +89,7 @@ function App() {
 						<input
 							type='checkbox'
 							id='uppercase-id'
+							checked={isUppercase}
 							onChange={() => {
 								setIsUppercase(prev => !prev)
 							}}
@@ -200,8 +101,7 @@ function App() {
 				<button
 					className='generate-button'
 					onClick={() => {
-						countChecks()
-						generatePass(passLength, isSpecialChar, isNumbers, isUppercase)
+						generatePass()
 						setIsPassGenerate(true)
 					}}
 				>
@@ -210,10 +110,28 @@ function App() {
 				{isPassGenerate && (
 					<div className='generated-form'>
 						<strong>Your password is:</strong>
-						<div className='generated-pass'>{newPass}</div>
+						<div className='generated-pass'>
+							{isPassShown ? newPass : hiddenPass}
+						</div>
 						<div className='pass-options'>
-							<button className='hideshow-pass'>Hide</button>
-							<button className='copy-pass'>Copy</button>
+							<button
+								className='hideshow-pass'
+								onClick={() => {
+									setIsPassShown(prev => !prev)
+									setHiddenPass('*'.repeat(passLength))
+								}}
+							>
+								{isPassShown ? 'Hide' : 'Show'}
+							</button>
+							<button
+								className='copy-pass'
+								onClick={() => {
+									navigator.clipboard.writeText(newPass.join(''))
+									alert('Your password was copied!')
+								}}
+							>
+								Copy
+							</button>
 						</div>
 					</div>
 				)}
